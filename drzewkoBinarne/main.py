634a0b3901aeb6e5652d97befda1613ae9b2de71
KarolKita <kkita@student.agh.edu.pl>
2022-04-07 20:20:40 +0200

    poprawienie komentarzy

--- drzewkoBinarne/main.py
+++ drzewkoBinarne/main.py
@@ -15,26 +15,29 @@ ilo_kolumn = 14  # ilosc kolumn w arkuszu
 atrybut = arkusz.cell(1, 1).value  # pierwszy atrybut w arkuszu
 
 # wczytanie danych do tablicy wiersz po wierszu z arkusza
-# gdzie w pierwszej kolumnie sa atrybuty
-# w drugiej kolumnie sa przeslanki
+# gdzie w pierwszej kolumnie sa przeslanki
+# w drugiej kolumnie sa atrybuty
 # a w kolumnach od 3 do 103 (?) sa przypadki
 for nr_wiersza in range(1, ilo_wierszy+1):
-    # wczytanie atrybutu
+    # wczytanie przeslanki
     if arkusz.cell(nr_wiersza, 1).value != var:
         atrybut = arkusz.cell(nr_wiersza, 1).value
         tablica[atrybut] = {}
 
-    # wczytanie przeslanki
+    # wczytanie atrybutu
     tablica[atrybut][arkusz.cell(nr_wiersza, 2).value] = []
 
-    # wczytanie przypadkow dla danej przeslanki (czyli '0' oraz '1')
+    # wczytanie przypadkow dla atrybutu (czyli '0' oraz '1')
     for wiersz in arkusz.iter_rows(min_row=nr_wiersza, min_col=3, max_row=nr_wiersza, max_col=ilo_kolumn):
         for przypadek in wiersz:
             tablica[atrybut][arkusz.cell(nr_wiersza, 2).value].append(przypadek.value)
 
 # testowe wypisanie wczytanej tablicy
-for atrybut in tablica:
-    print(atrybut, ": ")
-    for przeslanka in tablica[atrybut]:
-        print("     ", przeslanka, ": ", end=" ")
-        print(tablica[atrybut][przeslanka])
+for p in tablica:
+    print(p, ": ")
+    for a in tablica[p]:
+        print("     ", a, ": ", end=" ")
+        print(tablica[p][a])
+
+
+

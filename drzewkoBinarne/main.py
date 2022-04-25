38864baec99f54657fe3b2573530042eb3e85dc8
KarolKita <kkita@student.agh.edu.pl>
2022-04-25 22:24:05 +0200

    Dodanie funkcji liczacych entropie po zatwierdzeniu i zaprzeczemiu warunku

--- drzewkoBinarne/main.py
+++ drzewkoBinarne/main.py
@@ -49,20 +49,84 @@ def wypisz(tab):
 szukana = 'reklama'
 
 
-# liczenie entropi 'I'
-# *tab - nasza 'tablica' z danymi
+# liczenie entropi 'I' dla warunku j
+# tab - 'tablica' z danymi
 # szuk - szukana przeslanka
-# n - liczba kolumn z przypadkami
+# n - liczba kolumn z przypadkami (0,1)
 def entropia(tab, szuk, n):
     tab_ni = {}  # slownik z ilosciami przypadkow dla danego atrybutu
-    # zliczanie ilosci przypadkow
+    # zliczanie ilosci przypadkow dla atrybutu
     for atr in tab[szuk]:
         tab_ni[atr] = sum(tab[szuk][atr])
     # liczenie entropi 'I'
     entr = 0
     for atr in tab_ni:
         entr -= (tab_ni[atr] / n) * log(tab_ni[atr]/n, 2)
-    print('Entropia = ', entr)
+    return entr
 
 
-entropia(tablica, szukana, ilo_kolumn-2)
+# liczenie lacznej entopi po ocenie warunku j
+# tab - 'tablica' z danymi
+# szuk - szukana przeslanka
+# n - liczba kolumn z przypadkami (0,1)
+def laczna_entropia(tab, szuk, przes, war):
+    n = len(tab[przes][war])  # liczba przypadkow
+    nr = 0  # numer kolumny sprawdzanego przypadku
+    # tworzenie slownika do przechowywania ilosci przypadkow potwierdzajacych i zaprzeczajacych warunek j
+    tab_ni = {}
+    for atr in tab[szuk]:
+        tab_ni[atr] = {"n+": 0, "n-": 0}
+
+    # zliczanie ilosci przypadkow dla warunku war
+    # w tej petli bedziemy chodzic po przypadkach dla warunku war
+    # gdzie p_w to przypadek w nr-kolumnie
+    for p_w in tab[przes][war]:
+        # w tej sprawdzamy przypadki dla atrybutow z konkluzji w nr-kolumnie
+        for atr in tab[szuk]:
+            # szukamy atrybutow dla ktorych wartosc przypadku jest rowna '1'
+            if tab[szuk][atr][nr] == 1:
+                if p_w == 1:
+                    # zwiekszamy ilosc elementow potwierdzajacych warunek
+                    tab_ni[atr]['n+'] += 1
+                else:
+                    # zwiekszamy ilosc elementow zaprzeczających warunek
+                    tab_ni[atr]['n-'] += 1
+
+        nr += 1  # zwiekszamy numer kolumny
+
+    # liczenie entropi potwierdzajacych oraz zaprzeczajacych warunek
+    n_potw = sum(tab[przes][war])  # laczna ilosc elementow potwierdzajacych warunek
+    n_zaprz = n - n_potw  # laczna ilosc elementow zaprzeczajacych warunek
+    entr_potw = 0  # entropia po potwierdzniu warunku
+    entr_zaprz = 0  # entropia po zaprzeczeniu warunku
+
+    for atr in tab[szuk]:
+        # liczenie entropi po potwierdzeniu warunku
+        if tab_ni[atr]['n+'] > 0:
+            entr_potw -= (tab_ni[atr]['n+'] / n_potw) * log((tab_ni[atr]['n+'] / n_potw), 2)
+        # liczenie entropi po zaprzeczeniu warunku
+        if tab_ni[atr]['n-'] > 0:
+            entr_zaprz -= (tab_ni[atr]['n-'] / n_zaprz) * log((tab_ni[atr]['n-'] / n_zaprz), 2)
+
+    return (n_potw / n) * entr_potw + (n_zaprz / n) * entr_zaprz
+
+
+################################################################################################
+# najwieksza wartosc lacznej entropi
+entr_maks = -1
+atr_maks = ''
+
+# wartosc entropi
+e = entropia(tablica, szukana, ilo_kolumn-2)
+
+# wartosci lacznej entropi dla warunkow
+for przeslanka in tablica:
+    if przeslanka != szukana:
+        for a in tablica[przeslanka]:
+            if entr_maks < (e - laczna_entropia(tablica, szukana, przeslanka, a)):
+                entr_maks = e - laczna_entropia(tablica, szukana, przeslanka, a)
+                atr_maks = a
+            print(a, end=': ')
+            print(e - laczna_entropia(tablica, szukana, przeslanka, a))
+
+print('Najwiesza entropia: ', atr_maks)

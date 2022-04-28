2f02886f23ba8bbb2766e0d5e53167b3076552e1
KarolKita <kkita@student.agh.edu.pl>
2022-04-28 19:02:10 +0200

    Wstepna klasa drzewka

---
+++ drzewkoBinarne/drzewko_binarne.py
@@ -0,0 +1,7 @@
+class Drzewko:
+    def __init__(self, korzen=None, tak=None, nie=None, tabela=None):
+        self.korzen = korzen
+        self.tak = tak
+        self.nie = nie
+        self.tabela = tabela
+        

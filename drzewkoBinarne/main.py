fa1cc4fcfa0057aecc9807ec0c9ba34482f5bf41
KarolKita <kkita@student.agh.edu.pl>
2022-05-08 16:22:13 +0200

    FINAL VERSION

--- drzewkoBinarne/main.py
+++ drzewkoBinarne/main.py
@@ -4,8 +4,17 @@ from openpyxl import load_workbook
 import math
 # biblioteka sluzaca do graficznego przdstawienia drzewa
 import tkinter as tk
-# wlasna biblioteka zawierajaca klase implementujaca drzewko binarne
-from drzewko_binarne import Drzewko
+
+
+# klasa stworzona do przechowywania oraz tworzenia drzewa decyzyjnego
+class Drzewko:
+    def __init__(self, korzen):
+        self.korzen = korzen  # atrybut z najwieksza laczna entropia
+        self.przeslanka = None  # przeslanka dla atrybutu
+        self.tak = None  # "galaz tak" przechowuje atrybut/konkluzje
+        self.nie = None  # "galaz nie" przechowuje atrybut/konkluzje
+        self.tab_tak = None  # pole przechowuje tablice po podziale
+        self.tab_nie = None  # pole przechowuje tablice po podziale
 
 
 # ladowanie danych z excela
@@ -16,31 +25,31 @@ arkusz = wb.active
 tablica = {}
 
 # zmienne pomocnicze
-var = None
 ilo_wierszy = 21  # ilosc wierszy w arkuszu
 ilo_kolumn = 164  # ilosc kolumn w arkuszu
-atrybut = arkusz.cell(1, 1).value  # pierwszy atrybut w arkuszu
+przesl = arkusz.cell(1, 1).value  # pierwsza przeslanka w arkuszu
+szukana = 'Segment'  # szukana przeslanka
 
 # wczytanie danych do tablicy wiersz po wierszu z arkusza
 # gdzie w pierwszej kolumnie sa przeslanki
 # w drugiej kolumnie sa atrybuty
-# a w kolumnach od 3 do  164 sa przypadki
+# a w kolumnach od 3 do 164 sa przypadki
 for nr_wiersza in range(1, ilo_wierszy+1):
     # wczytanie przeslanki
-    if arkusz.cell(nr_wiersza, 1).value != var:
-        atrybut = arkusz.cell(nr_wiersza, 1).value
-        tablica[atrybut] = {}
+    if arkusz.cell(nr_wiersza, 1).value is not None:
+        przesl = arkusz.cell(nr_wiersza, 1).value
+        tablica[przesl] = {}  # zagniezdzanie slownika do przechowywania atrybutow
 
     # wczytanie atrybutu
-    tablica[atrybut][arkusz.cell(nr_wiersza, 2).value] = []
+    tablica[przesl][arkusz.cell(nr_wiersza, 2).value] = []
 
-    # wczytanie przypadkow dla atrybutu (czyli '0' oraz '1')
+    # wczytanie przypadkow
     for wiersz in arkusz.iter_rows(min_row=nr_wiersza, min_col=3, max_row=nr_wiersza, max_col=ilo_kolumn):
         for przypadek in wiersz:
-            tablica[atrybut][arkusz.cell(nr_wiersza, 2).value].append(przypadek.value)
+            tablica[przesl][arkusz.cell(nr_wiersza, 2).value].append(przypadek.value)
 
 
-# testowe wypisanie tablicy
+# wypisanie tablicy
 def wypisz(tab):
     for p in tab:
         print(p, ": ")
@@ -49,16 +58,12 @@ def wypisz(tab):
             print(tab[p][a])
 
 
-# szukana przeslanka
-szukana = 'Segment'
-
-
 # liczenie entropi 'I' dla warunku j
 # tab - 'tablica' z danymi
 # szuk - szukana przeslanka
 def entropia(tab, szuk):
     tab_ni = {}  # slownik z ilosciami przypadkow dla danego atrybutu
-    n = len(tab['Segment']['A'])  # ilosc kolumn z przypadkami
+    n = len(tab['Segment']["A"])  # ilosc kolumn z przypadkami
     # zliczanie ilosci przypadkow dla atrybutu
     for atr in tab[szuk]:
         tab_ni[atr] = sum(tab[szuk][atr])
@@ -67,25 +72,27 @@ def entropia(tab, szuk):
     for atr in tab_ni:
         if tab_ni[atr] > 0:
             entr -= (tab_ni[atr] / n) * math.log(tab_ni[atr]/n, 2)
-    return entr
+
+    return entr  # zwracana wartosc entropi dla warunku j
 
 
-# liczenie entopi potwierdzajacych i zatwierdzajacych warunek j
+# liczenie entropi potwierdzajacych i zaprzeczajacych warunek j
 # tab - 'tablica' z danymi
 # szuk - szukana przeslanka
-# n - liczba kolumn z przypadkami (0,1)
-def entr_potw_zaprz(tab, szuk, przes, war):
-    n = len(tab[przes][war])  # liczba przypadkow
+# przes - przeslanka
+# szuk_atr - atrybut dla ktorego liczona jest entropia
+def entr_potw_zaprz(tab, szuk, przes, szuk_atr):
+    n = len(tab[przes][szuk_atr])  # liczba przypadkow
     nr = 0  # numer kolumny sprawdzanego przypadku
     # tworzenie slownika do przechowywania ilosci przypadkow potwierdzajacych i zaprzeczajacych warunek j
     tab_ni = {}
     for atr in tab[szuk]:
         tab_ni[atr] = {"n+": 0, "n-": 0}
 
-    # zliczanie ilosci przypadkow dla warunku war
-    # w tej petli bedziemy chodzic po przypadkach dla warunku war
+    # zliczanie ilosci przypadkow dla przekazanego atrybutu
+    # w tej petli bedziemy chodzic po przypadkach atrybutu
     # gdzie p_w to przypadek w nr-kolumnie
-    for p_w in tab[przes][war]:
+    for p_w in tab[przes][szuk_atr]:
         # w tej sprawdzamy przypadki dla atrybutow z konkluzji w nr-kolumnie
         for atr in tab[szuk]:
             # szukamy atrybutow dla ktorych wartosc przypadku jest rowna '1'
@@ -100,7 +107,7 @@ def entr_potw_zaprz(tab, szuk, przes, war):
         nr += 1  # zwiekszamy numer kolumny
 
     # liczenie entropi potwierdzajacych oraz zaprzeczajacych warunek
-    n_potw = sum(tab[przes][war])  # laczna ilosc elementow potwierdzajacych warunek
+    n_potw = sum(tab[przes][szuk_atr])  # laczna ilosc elementow potwierdzajacych warunek
     n_zaprz = n - n_potw  # laczna ilosc elementow zaprzeczajacych warunek
     entr_potw = 0  # entropia po potwierdzniu warunku
     entr_zaprz = 0  # entropia po zaprzeczeniu warunku
@@ -113,13 +120,14 @@ def entr_potw_zaprz(tab, szuk, przes, war):
         if tab_ni[atr]['n-'] > 0:
             entr_zaprz -= (tab_ni[atr]['n-'] / n_zaprz) * math.log((tab_ni[atr]['n-'] / n_zaprz), 2)
 
+    # zwrocenie sumy entropi potwierdzajacych oraz zaprzeczajacych warunek j
     return (n_potw / n) * entr_potw + (n_zaprz / n) * entr_zaprz
 
 
-# funkcja zwracajaca atrybut z nawieksza laczna entropia
+# funkcja zwracajaca atrybut i przeslanke z nawieksza laczna entropia
 # tab - 'tablica' z danymi
 # szuk - szukana przeslanka
-# p - dodatkowy parametr ktory zawiera przeslanke rodzica
+# p - dodatkowy parametr ktory zawiera przeslanke "rodzica"
 def max_laczna_entropia(tab, szuk, p=None):
     entr_maks = -1  # najwieksza laczna entropia
     atr_maks = ''  # atrybut z najwieksza entropia
@@ -128,8 +136,9 @@ def max_laczna_entropia(tab, szuk, p=None):
     # wartosc entropi
     entr = entropia(tab, szuk)
 
-    # liczenie lacznej wartosci entropi
+    # liczenie oraz szukanie najwiekszej lacznej wartosci entropi
     for przes in tab:
+        # omijanie konkluzji oraz unikanie powtorzen atrybutow z jednej przeslanki w jednym wezle
         if przes != szuk and przes != p:
             for atr in tab[przes]:
                 # szukanie atrybutu z najwieksza wartoscia lacznej entropi
@@ -138,12 +147,13 @@ def max_laczna_entropia(tab, szuk, p=None):
                     atr_maks = atr
                     przes_maks = przes
 
+    # zwrocenie atrybutu oraz przeslanki z najwieksza laczna entropia
     return atr_maks, przes_maks
 
 
 # funkcja dzielaca tabele
 # tab 'tablica' z danymi
-# przes_p przeslanka dla ktore atrybut posiada najwieksza laczna entropie
+# przes_p przeslanka dla ktorej atrybut posiada najwieksza laczna entropie
 # atr_p atrybut na podstawie ktorego dzielimy tabele
 def podzial_tab(tab, przes_p, atr_p):
     tab_tak = {}  # tablica z danymi potwierdzajacymi warunek
@@ -171,6 +181,7 @@ def podzial_tab(tab, przes_p, atr_p):
                     tab_nie[przes][atr].append(tab[przes][atr][nr_kol])
         nr_kol += 1  # zwiekszanie numeru kolumny
 
+    # zwrocenie tablicy z potwierdzajacych i zaprzeeczajacych warunek
     return tab_tak, tab_nie
 
 
@@ -184,16 +195,18 @@ def sprawdz(tab, szuk):
 
 
 # funkcja zwraca konkluzje z samymi '1' w przypadkach
+# tab - 'tablica' z danymi
+# szuk - szukana przeslanka
 def zwroc_konkluzje(tab, szuk):
-    for atr in tab[szuk]:
-        if len(tab[szuk][atr]) == sum(tab[szuk][atr]):
-            return atr
+    for konluzja in tab[szuk]:
+        if len(tab[szuk][konluzja]) == sum(tab[szuk][konluzja]):
+            return konluzja
 
 
 # tworzenie korzenia
-atr_temp, przes_temp = max_laczna_entropia(tablica, szukana)
-drzewo = Drzewko(atr_temp)
-drzewo.przeslanka = przes_temp
+atr_korzen, przes_korz = max_laczna_entropia(tablica, szukana)
+drzewo = Drzewko(atr_korzen)
+drzewo.przeslanka = przes_korz
 
 
 # funkcja tworzaca drzewo
@@ -201,33 +214,40 @@ drzewo.przeslanka = przes_temp
 # tab - 'tablica' z danymi
 # szuk - szukana przeslanka
 def tworz_drzewo(korz, tab, szuk):
-    # podzial tabeli
+    # podzial tabeli 'rodzica'
     korz.tab_tak, korz.tab_nie = podzial_tab(tab, korz.przeslanka, korz.korzen)
 
     # tworzenie kolejnych galezi drzewa
-    # galezie z 'tak'
+    # galezie 'tak'
+    # sprawdzanie czy w otrzymanej tablicy wystepuje jedna wartosc konkluzji
     if sprawdz(korz.tab_tak, szuk) == 1:
+        # dodawanie konkluzji do drzewa
         korz.tak = zwroc_konkluzje(korz.tab_tak, szuk)
     else:
+        # szukanie atrybutu z najwieksza laczna entropia
         temp_t = max_laczna_entropia(korz.tab_tak, szuk)
         # sprawdzenie czy aktualna przeslanka jest rozna od przeslanki rodzica
         if temp_t[1] == korz.przeslanka:
             temp_t = max_laczna_entropia(korz.tab_tak, szuk, temp_t[1])
+        # tworzenie nowego obiektu Drzewka
         korz.tak = Drzewko(temp_t[0])
         korz.tak.przeslanka = temp_t[1]
+        # wywolanie rekurencyjne funkcji tworz_drzewo()
         tworz_drzewo(korz.tak, korz.tab_tak, szuk)
-    # galezie z 'nie'
+
+    # galezie 'nie'
+    # tworzona analogicznie jak "galaz tak"
     if sprawdz(korz.tab_nie, szuk) == 1:
         korz.nie = zwroc_konkluzje(korz.tab_nie, szuk)
     else:
         temp_n = max_laczna_entropia(korz.tab_nie, szuk)
-        # sprawdzenie czy aktualna przeslanka jest rozna od przeslanki rodzica
         if temp_n[1] == korz.przeslanka:
             temp_n = max_laczna_entropia(korz.tab_tak, szuk, temp_n[1])
         korz.nie = Drzewko(temp_n[0])
         korz.nie.przeslanka = temp_n[1]
         tworz_drzewo(korz.nie, korz.tab_nie, szuk)
 
+    # zwrocenie obiektu klasy Drzewko
     return korz
 
 
@@ -261,22 +281,23 @@ def dane_do_rysowania(korz, d, x, y, wart=None):
     else:
         d.append([korz.nie, x + 100, y + 75, 0])
 
+    # zwrocenie listy danych
     return d
 
 
 # slownik zawierajacy nazwy etykiet
-nazwy = {"15 <": "dystans < 15",
-         "15 - 35": "dystans miedzy 15 a 35",
-         "35 >=": "dystans >= 35",
-         "= 1": "liczba przewozonych osob = 1",
-         "= 2": "liczba przewozonych osob = 2",
+nazwy = {"15 <": "dystans < 15 km",
+         "15 - 35": "dystans miedzy 15 a 35 km",
+         "35 >=": "dystans >= 35 km",
+         " 1": "liczba przewozonych osob = 1",
+         " 2": "liczba przewozonych osob = 2",
          "3 >=": "liczba przewozonych osob >= 3",
-         "50 <": "wielkosc bagazu < 50",
-         "50 - 100": "wielkosc bagazu miedzy 50 a 100",
-         "100 >=": "wielkosc bagazu >= 100",
-         "<= 50": "srednia predkosc <= 50",
-         "50 - 70": "srednia predkosc miedzy 50 a 70",
-         "70 >=": "srednia predkosc >= 70",
+         "50 <": "wielkosc bagazu < 50 kg",
+         "50 - 100": "wielkosc bagazu miedzy 50 a 100 kg",
+         "100 >=": "wielkosc bagazu >= 100 kg",
+         "<= 50": "srednia predkosc <= 50 km/h",
+         "50 - 70": "srednia predkosc miedzy 50 a 70 km/h",
+         "70 >=": "srednia predkosc >= 70 km/h",
          "ekspresowa": "droga ekspresowa",
          "główna": "droga glowna",
          "lokalna": "droga lokalna",
@@ -287,15 +308,15 @@ nazwy = {"15 <": "dystans < 15",
          "D": "auto rodzinne",
          "J": "auto terenowe"}
 
-# tworzenie danych do graficznego przedstawienia drzewa
+# wywołanie funkcji
 dane_do_rysowania(drzewo, dane, 425, 0)
 
 # graficzne przedstawienie drzewa
-# tworzenie okna
+# tworzenie okna tkinter
 okno = tk.Tk()
 
 # dodanie tytulu i rozmiarow okna
-okno.title("Drzewko decyzyjne")
+okno.title("Drzewko decyzyjne - dobór typu samochodu do potrzeb")
 okno.geometry("1600x800")
 okno.resizable(False, False)
 
